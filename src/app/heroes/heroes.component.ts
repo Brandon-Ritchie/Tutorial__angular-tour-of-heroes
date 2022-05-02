ac92d0b5f7d7e4d6a0156ca2eb53fe3e3712f849
Brandon Ritchie <britchie@avi>
2022-05-02 15:17:32 -0400

    finished Display a List section of Tour of Heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../Hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -9,9 +10,12 @@ import { Hero } from '../Hero';
 export class HeroesComponent implements OnInit {
   constructor() {}
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
+  heroes = HEROES;
+
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
   ngOnInit(): void {}
 }
